0ca2df5c2416061a3a9b08444cc69e89a99088d8
GabrielMouraKT <gabrieljknd@gmail.com>
2023-09-10 19:42:17 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/floatingbottomnavbar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.floatingbottomnavbar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,30 @@
+import 'package:floating_bottom_navigation_bar/floating_bottom_navigation_bar.dart';
+import 'package:flutter/material.dart';
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      bottomNavigationBar: FloatingNavbar(
+        onTap: (int val) {
+          //returns tab id which is user tapped
+        },
+        currentIndex: 2,
+        items: [
+          FloatingNavbarItem(icon: Icons.home, title: 'Home'),
+          FloatingNavbarItem(icon: Icons.explore, title: 'Explore'),
+          FloatingNavbarItem(icon: Icons.chat_bubble_outline, title: 'Chats'),
+          FloatingNavbarItem(icon: Icons.settings, title: 'Settings'),
+        ],
+      ),
+    );
+  }
+}
+//https://pub.dev/packages/floating_bottom_navigation_bar
